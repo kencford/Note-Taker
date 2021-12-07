7ce9333e52ff21eb9cf515da49b4a0651d6e4d5b
Ken Crawford <KenCrawford@Fastmail.com>
2021-12-07 18:33:17 -0500

    after working with tutor

---
+++ routes/apiRoutes.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const fs = require("fs");
+const dbFile = require("../db/db.json");
+const uniqid = require("uniqid");
+
+// Note: 
+// const sample = require("sample");
+// if use uniqid (better?) change "sample" to uniqid
+router.get("/api/notes", (req, res) => {
+    fs.readFile(dbFile, "utf8", (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });   
+});
+
+
+// posting a note
+router.post("/api/notes", (req, res) => {
+    fs.readFile(dbFile, "utf8", (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            var noteId = uniqid();
+            var id = "id";
+            noteNew[id] = noteId;
+            var jsonDB = JSON.parse(data);
+            var noteNew = req.body;
+            fs.writeFile("db/db.json", JSON.stringify(jsonDB), (err) => {
+                if (err) throw err;
+                res.json(jsonDB);
+            })
+            }
+            
+        });
+    });
+
+// delete a note}
+
+
+
+
+module.exports = router;
+
